03db659c0359d92e12b1263139a094b9e8fcba76
GSonofNun <sonofnun@outlook.com>
2014-09-15 12:23:47 -0400

    Adding Namespace class

---
+++ inbox_net/DataModel/Namespace.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inbox_net.DataModel
+{
+    [ImplementPropertyChanged]
+    public class Namespace
+    {
+        [JsonProperty("id")]
+        public string ID { get; set; }
+
+        [JsonProperty("object")]
+        public string Object { get; set; }
+
+        [JsonProperty("namespace_id")]
+        public string Namespace_ID { get; set; }
+
+        [JsonProperty("account_id")]
+        public string Account_ID { get; set; }
+
+        [JsonProperty("email_address")]
+        public string Email_Address { get; set; }
+
+        [JsonProperty("provider")]
+        public string Provider { get; set; }
+
+    }
+}
