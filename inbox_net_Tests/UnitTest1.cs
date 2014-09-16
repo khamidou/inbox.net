424750a2971ea5fe42d0716a4eb7ead3151b87ac
GSonofNun <sonofnun@outlook.com>
2014-09-16 09:20:03 -0400

    Added get namespace methods

---
+++ inbox_net_Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using inbox_net;
+
+namespace inbox_net_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public async void TestMethod1()
+        {
+            InboxClient client = new InboxClient("http://inboxappmachine.cloudapp.net:5555/");
+            await client.GetFirstNamespace();
+        }
+    }
+}
